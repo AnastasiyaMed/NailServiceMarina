6eda862e85314b51e52585638920c739509f7c47
Anastasiya <hlystya@gmail.com>
2017-09-19 00:54:56 +0300

    refactor modules

--- data/src/androidTest/java/by/medvedeva/anastasiya/data/ExampleInstrumentedTest.java
+++ data/src/androidTest/java/by/medvedeva/anastasiya/data/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("by.medvedeva.anastasiya.data", appContext.getPackageName());
+        assertEquals("by.medvedeva.anastasiya.data.test", appContext.getPackageName());
     }
 }
--- domain/src/androidTest/java/by/medvedeva/anastasiya/domain/ExampleInstrumentedTest.java
+++ domain/src/androidTest/java/by/medvedeva/anastasiya/domain/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("by.medvedeva.anastasiya.domain", appContext.getPackageName());
+        assertEquals("by.medvedeva.anastasiya.domain.test", appContext.getPackageName());
     }
 }
