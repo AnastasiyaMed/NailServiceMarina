e7e9fa43cae1823ea1d8be79ed780c73b9068416
Anastasiya <hlystya@gmail.com>
2017-09-25 19:52:20 +0300

    fabric added

--- presentation/src/main/java/by/medvedeva/anastasiya/nailservicemarina/activities/CalendarActivity.java
+++ presentation/src/main/java/by/medvedeva/anastasiya/nailservicemarina/activities/CalendarActivity.java
@@ -1,8 +1,16 @@
 package by.medvedeva.anastasiya.nailservicemarina.activities;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 import by.medvedeva.anastasiya.nailservicemarina.R;
 import by.medvedeva.anastasiya.nailservicemarina.base.BaseActivity;
@@ -20,8 +28,15 @@ public class CalendarActivity extends BaseActivity {
         final CalendarViewModel viewModel = new CalendarViewModel(this);
         this.viewModel = viewModel;
         ActivityCalendarBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_calendar);
+
         binding.setViewModel(viewModel);
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)
+                .build();
+        Fabric.with(fabric);
 
     }
 }
